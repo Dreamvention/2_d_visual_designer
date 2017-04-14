4018de4a30af6c2313950370aeffdff6cb91d5c9
Alexandr Krasny <php4.drvn@gmail.com>
2017-04-14 15:29:22 +0300

    Fix bugs

--- catalog/controller/event/d_visual_designer.php
+++ catalog/controller/event/d_visual_designer.php
@@ -54,7 +54,15 @@ class ControllerEventDVisualDesigner extends Controller
             foreach ($data['products'] as $key => $value) {
                 $product_info = $this->model_catalog_product->getProduct($value['product_id']);
                 $data['products'][$key]['description'] = $this->{'model_module_'.$this->codename}->getText($product_info['description']);
-                $data['products'][$key]['description'] = utf8_substr(strip_tags(html_entity_decode($data['products'][$key]['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get($this->config->get('config_theme') . '_product_description_length')) . '..';
+                if(VERSION>='2.2.0.0')
+                {
+                    $data['products'][$key]['description'] = utf8_substr(strip_tags(html_entity_decode($data['products'][$key]['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get($this->config->get('config_theme') . '_product_description_length')) . '..';
+                }
+                else
+                {
+                    $data['products'][$key]['description'] = utf8_substr(strip_tags(html_entity_decode($data['products'][$key]['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('config_product_description_length')) . '..';
+                }
+                
             }
         }
     }
