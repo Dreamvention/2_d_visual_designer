3f127d1b77e185d1e1f00a6f5de10ca47deb2694
alexandrdrvn <php4.drvn@gmail.com>
2018-07-12 16:26:11 +0300

    Fix bugs

--- catalog/view/theme/default/javascript/d_visual_designer/model/style.js
+++ catalog/view/theme/default/javascript/d_visual_designer/model/style.js
@@ -29,13 +29,13 @@
         var block_info = this.getState().blocks[data.designer_id][data.block_id]
 
         var baseSelector = '[data-id='+data.block_id+'] .block-content[data-is="vd-block-'+block_info.type+'"]'
-        $('head').find('style#'+data.designer_id+'-'+data.block_id).remove();
+        $('head').find('style#media-'+data.designer_id+'-'+data.block_id).remove();
         var medias = {
             'phone': '@media screen and (max-width: 767px)',
             'tablet': '@media screen and (min-width: 768px) and (max-width: 991px)',
             'desktop': '@media screen and (min-width: 992px)'
         }
-        var styleTag = '<style id="'+data.designer_id+'-'+data.block_id+'">';
+        var styleTag = '<style id="media-'+data.designer_id+'-'+data.block_id+'">';
 
         for(var key in data.styles) {
             styleTag += medias[key]+'{';
